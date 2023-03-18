9305614b45c0cbff3d917f916f1d8319f4a60a2b
Thiago-Portella <thiagoportellaferreira@gmail.com>
2023-03-18 12:37:59 -0300

    starting project

---
+++ src/main/kotlin/com/love/calculator/love_cauculator/LoveCauculatorApplication.kt
@@ -0,0 +1,11 @@
+package com.love.calculator.love_cauculator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoveCauculatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<LoveCauculatorApplication>(*args)
+}
---
+++ src/test/kotlin/com/love/calculator/love_cauculator/LoveCauculatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.love.calculator.love_cauculator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LoveCauculatorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
